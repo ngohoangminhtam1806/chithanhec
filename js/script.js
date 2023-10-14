d20779164d180bcbe1ceceeedde65e2d68da2539
Tam Ngo <131700810@dntu.edu.vn>
2023-10-14 14:51:25 +0700

    Create solutions + project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// function lines(){
+// 	let sizeW = Math.random() * 12;
+// 	let e = document.createElement('div');
+// 	e.setAttribute('class','circle');
+// 	document.body.appendChild(e);
+
+// 	e.style.width = 2 +sizeW+ 'px';
+// 	e.style.left = Math.random() * + innerWidth + 'px';
+
+// 	setTimeout(function(){
+// 		document.body.removeChild(e)
+// 	},5000);
+// }
+
+// setInterval(function(){
+// 	lines();
+// },200);
+
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
@@ -19,8 +37,18 @@ const sr = ScrollReveal ({
 	reset: true,
 });
 
+// Index
 sr.reveal('.hero-text', {deplay: 200, origin: 'top'});
-sr.reveal('.hero-introduce', {deplay: 200, origin: 'top'});
 sr.reveal('.hero-img', {deplay: 450, origin: 'top'});
 sr.reveal('.icons', {deplay: 500, origin: 'left'});
 sr.reveal('.scroll-down', {deplay: 500, origin: 'right'});
+
+// Introduce
+sr.reveal('.hero-introduce', {deplay: 200, origin: 'top'});
+
+// Project
+sr.reveal('.hero-project .logo h2', {deplay: 200, origin: 'bottom'});
+sr.reveal('.hero-project .logo h6', {deplay: 200, origin: 'left'});
+sr.reveal('.hero-project .logo img', {deplay: 200, origin: 'right'});
+sr.reveal('.hero-project .content-flex-right', {deplay: 200, origin: 'right'});
+sr.reveal('.hero-project .content-flex-left', {deplay: 200, origin: 'left'});
